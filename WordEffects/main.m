393e1e32ac43a4a8f1cd6fffbb63b7c2cf99c817
Sam Ogunyowa <mailsamog@gmail.com>
2017-06-26 19:48:33 -0400

    Initial Commit

---
+++ WordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by Endeavour2 on 6/26/17.
+//  Copyright © 2017 SamOg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
