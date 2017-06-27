0ceccab5768b4b141ad7b4ed721c05bba19269fd
Sam Ogunyowa <mailsamog@gmail.com>
2017-06-26 20:23:24 -0400

    First commit

--- WordEffects/main.m
+++ WordEffects/main.m
@@ -10,8 +10,23 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        char inputChars[256]; //255 long unit of character
+        printf("Please type in a text as input \n");
+        fgets(inputChars, 255, stdin);
+        
+        printf("Your text is: %s\n", inputChars); //print out the user's text
+        
+        //convert char array to an NSString object
+        NSString *inputString = [[NSString stringWithUTF8String:inputChars] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+        
+        NSLog(@" line %d: The string you entered is: %@",__LINE__, inputString);
+        
+        
+        
+        
+        
+        
     }
     return 0;
 }
